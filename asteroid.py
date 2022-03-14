1468cd8b3cd196545908848d5a14e13f1cb3914b
victorDias14 <victodm14@gmail.com>
2022-03-14 19:35:54 -0300

    Primeiro

---
+++ asteroid.py
@@ -0,0 +1,22 @@
+import random
+import pygame
+
+
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+        self.image = pygame.image.load('data/meteorBrown_small1.png')
+        self.image = pygame.transform.scale(self.image, [100, 100])
+        self.rect = pygame.Rect(50, 50, 100, 100)
+
+        self.rect.x = 840 + random.randint(1, 400)
+        self.rect.y = random.randint(1, 400)
+
+        self.speed = 1 + random.random() * 2
+
+    def update(self, *args):
+        self.rect.x -= self.speed
+
+        if self.rect.right < 0:
+            self.kill()
